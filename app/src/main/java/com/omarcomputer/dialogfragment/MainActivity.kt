12d84dfbeaa6ef38e315cf49c04339e41a95fd0a
Omar OUGHZAL <omar.oughzal@gmail.com>
2023-10-31 16:30:04 +0100

    Initial commit

---
+++ app/src/main/java/com/omarcomputer/dialogfragment/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.omarcomputer.dialogfragment
+
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
+import android.widget.TimePicker
+import java.time.LocalDate
+import java.time.LocalTime
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var dateText : TextView
+    private lateinit var btnDate : Button
+    private lateinit var btnTime : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        dateText = findViewById(R.id.dateText)
+        btnDate = findViewById(R.id.btnDate)
+        btnTime = findViewById(R.id.btnTime)
+        btnDate.setOnClickListener {
+            val today = LocalDate.now()
+            val datePick =DatePickerDialog(this,{ datePicker: DatePicker, year: Int, month: Int, day: Int ->
+                dateText.text ="$day/$month/$year"
+            },today.year,today.monthValue,today.dayOfMonth)
+            datePick.show()
+        }
+
+        btnTime.setOnClickListener {
+            val now = LocalTime.now()
+            val timePicker = TimePickerDialog(this,{ _: TimePicker, heures: Int, minutes: Int ->
+                dateText.text ="$heures:$minutes"
+            },now.hour,now.minute,true)
+            timePicker.show()
+        }
+    }
+}
